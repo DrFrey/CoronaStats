00710953bb5b0d860721cbd8d18fc6e2c70ec520
DrFrey <avorovatov@gmail.com>
2021-02-23 22:23:03 +0300

    added flags

--- app/src/main/java/com/example/coronastats/MainActivity.kt
+++ app/src/main/java/com/example/coronastats/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.coronastats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blongho.country_data.World
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        World.init(applicationContext)
     }
 }
