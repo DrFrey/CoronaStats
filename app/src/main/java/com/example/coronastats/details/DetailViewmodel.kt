8b44b6b67aa76d8c911fd93abe89343306ec0b73
DrFrey <avorovatov@gmail.com>
2021-02-24 10:50:31 +0300

    removed flags

--- app/src/main/java/com/example/coronastats/details/DetailViewmodel.kt
+++ app/src/main/java/com/example/coronastats/details/DetailViewmodel.kt
@@ -1,15 +1,9 @@
 package com.example.coronastats.details
 
 import android.app.Application
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
-import android.util.Log
-import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.blongho.country_data.World
 import com.example.coronastats.network.CountryData
 
 class DetailViewmodel(countryData: CountryData, application: Application): AndroidViewModel(application) {
@@ -17,10 +11,7 @@ class DetailViewmodel(countryData: CountryData, application: Application): Andro
     val selectedCountry: LiveData<CountryData>
         get() = _selectedCountry
 
-    val countryFlag: Drawable?
-
     init {
         _selectedCountry.value = countryData
-        countryFlag = ResourcesCompat.getDrawable(application.resources, World.getFlagOf(countryData.country), null)
     }
 }
